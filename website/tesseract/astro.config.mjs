e7b1b25ea7c69bfdd5510af71d1932ec911a02ab
Marcel Claus <geckse@gmx.de>
2026-04-06 21:04:23 +0200

    we dont want our marketing websites in git

--- website/tesseract/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://tesseract.dev',
-  output: 'static',
-  integrations: [svelte(), tailwind()],
-});
